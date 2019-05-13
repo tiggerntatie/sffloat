c3ed751ef0821ae0730dfee588040d8e69cca61c
Eric Dennison <ericd@netdenizen.com>
2019-05-13 14:05:17 -0400

    Updated from Brython Server: 5/13/2019 2:05:16 PM

--- sffloat.py
+++ sffloat.py
@@ -13,11 +13,81 @@ class sffloat:
             self.sf = sigfigs
             self.val = float(value)
             
+def to_precision(x,p):
+    """
+    returns a string representation of x formatted with a precision of p
+    
+    Based on implementation by Randle Taylor:
+    http://randlet.com/blog/python-significant-figures-format/
+    
+    Randle, in turn, credits:
+    Based on the webkit javascript implementation taken from here:
+    https://code.google.com/p/webkit-mirror/source/browse/JavaScriptCore/kjs/number_object.cpp
+    """
+
+    x = float(x)
+
+    if x == 0.:
+        return "0." + "0"*(p-1)
+
+    out = []
+
+    if x < 0:
+        out.append("-")
+        x = -x
+
+    e = int(math.log10(x))
+    tens = math.pow(10, e - p + 1)
+    n = math.floor(x/tens)
+
+    if n < math.pow(10, p - 1):
+        e = e -1
+        tens = math.pow(10, e - p+1)
+        n = math.floor(x / tens)
+
+    if abs((n + 1.) * tens - x) <= abs(n * tens -x):
+        n = n + 1
+
+    if n >= math.pow(10,p):
+        n = n / 10.
+        e = e + 1
+
+    m = "%.*g" % (p, n)
+
+    if e < -2 or e >= p:
+        out.append(m[0])
+        if p > 1:
+            out.append(".")
+            out.extend(m[1:p])
+        out.append('e')
+        if e > 0:
+            out.append("+")
+        out.append(str(e))
+    elif e == (p -1):
+        out.append(m)
+    elif e >= 0:
+        out.append(m[:e+1])
+        if e+1 < len(m):
+            out.append(".")
+            out.extend(m[e+1:])
+    else:
+        out.append("0.")
+        out.extend(["0"]*-(e+1))
+        out.append(m)
+
+    return "".join(out)
+
     def __repr__(self):
         if self.sf is None:
             return "sffloat({0})".format(self.val)
         else:
             return "sffloat({0},{1})".format(self.val, self.sf)
+            
+    def __str__(self):
+        if self.sf is None:
+            return str(self.val)
+        else
+            return to_precision (self.val, self.sf)
 
     def __float__(self):
         return self.val
@@ -43,7 +113,7 @@ class sffloat:
         """
         Implements reflected multiplication.
         """
-        return sffloat(sffloat(other).val * self.val)
+        return self * other
     
     """
     __sub__(self, other)
@@ -105,5 +175,5 @@ a = sffloat(1.0,2)
 b = sffloat(2.0,3)
 print(a*b)
 print(type(a*b))
-print(a*1.0)
-print(1.0*a)
+print(b*1)
+print(2*b)
