cfdfff9a3b50b26a26762ce71f2d3451d245977c
Eric Dennison <ericd@netdenizen.com>
2019-05-13 14:40:48 -0400

    Updated from Brython Server: 5/13/2019 2:40:48 PM

--- sffloat.py
+++ sffloat.py
@@ -26,7 +26,7 @@ class sffloat:
         if self.sf is None:
             return str(self.val)
         else:
-            return "{0e.{1}}".format(self.val, self.sf)
+            return "{0e.{1}}".format(self.val, self.sf-1)
 
     def __float__(self):
         return self.val
