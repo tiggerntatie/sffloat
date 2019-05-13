dfab50e7e0ae7d9a1d7a8936338cc206ecf98359
Eric Dennison <ericd@netdenizen.com>
2019-05-13 13:24:06 -0400

    Updated from Brython Server: 5/13/2019 1:24:06 PM

--- sffloat.py
+++ sffloat.py
@@ -18,6 +18,12 @@ class sffloat:
         Implements multiplication.
         """
         return sffloat(self.val * sffloat(other).val)
+
+    def __rmul__(self, other):
+        """
+        Implements reflected multiplication.
+        """
+        return sffloat(sffloat(other).val * self.val)
     
     """
     __sub__(self, other)
@@ -44,6 +50,34 @@ class sffloat:
     Implements bitwise or using the | operator.
     __xor__(self, other)
     Implements bitwise xor using the ^ operator.
+    
+    
+    __radd__(self, other)
+    Implements reflected addition.
+    __rsub__(self, other)
+    Implements reflected subtraction.
+    __rfloordiv__(self, other)
+    Implements reflected integer division using the // operator.
+    __rdiv__(self, other)
+    Implements reflected division using the / operator.
+    __rtruediv__(self, other)
+    Implements reflected true division. Note that this only works when from __future__ import division is in effect.
+    __rmod__(self, other)
+    Implements reflected modulo using the % operator.
+    __rdivmod__(self, other)
+    Implements behavior for long division using the divmod() built in function, when divmod(other, self) is called.
+    __rpow__
+    Implements behavior for reflected exponents using the ** operator.
+    __rlshift__(self, other)
+    Implements reflected left bitwise shift using the << operator.
+    __rrshift__(self, other)
+    Implements reflected right bitwise shift using the >> operator.
+    __rand__(self, other)
+    Implements reflected bitwise and using the & operator.
+    __ror__(self, other)
+    Implements reflected bitwise or using the | operator.
+    __rxor__(self, other)
+    Implements reflected bitwise xor using the ^ operator.    
     """
         
 
@@ -53,3 +87,5 @@ print(a*b)
 print(type(a*b))
 print(a.sf)
 print(b.sf)
+print(a*1.0)
+print(1.0*a)
