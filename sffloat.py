683f5dc883ccd37261a2eeaf673247c6c1b6fe4a
Eric Dennison <ericd@netdenizen.com>
2019-05-13 13:07:57 -0400

    Updated from Brython Server: 5/13/2019 1:07:56 PM

--- sffloat.py
+++ sffloat.py
@@ -1,12 +1,14 @@
 class sffloat(float):
     
-    def __new__(self, value, sigfigs=None):
-        return float.__new__(self, value)
+    def __new__(cls, value, sigfigs=None):
+        print("new")
+        return super().__new__(cls, value)
 
     def __init__(self, value, sigfigs=None):
+        print("init")
         float.__init__(value)
         self.sf = sigfigs
 
 x = sffloat(1.0)
-print(type(x))
-print(x)
+#print(type(x))
+#print(x)
