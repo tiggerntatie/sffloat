e7b8336cf1ea4ec8aa595b845ded53031a1adf42
Eric Dennison <ericd@netdenizen.com>
2019-05-13 10:18:54 -0400

    Updated from Brython Server: 5/13/2019 10:18:54 AM

--- sffloat.py
+++ sffloat.py
@@ -1,1 +1,6 @@
-
+class sffloat(float):
+    def __new__(self, value, extra):
+        return float.__new__(self, value)
+    def __init__(self, value, extra):
+        float.__init__(value)
+        self.extra = extra
